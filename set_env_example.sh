0e768a8753e187f41799c26fd9242ba0c7727a82
Markus Leist <markus@lei.st>
2016-06-30 16:22:58 +0200

    example for env setup

---
+++ set_env_example.sh
@@ -0,0 +1,11 @@
+# general settings
+export GTTG_TIMEZONE=Europe/Berlin
+
+# display settings
+export GTTG_TITLE=My GTTG Administration
+
+# template settings
+export GTTG_PPTX_TEMPLATE="...../m_tmpl_gttg1.pptx"
+export GTTG_PPTX_LAYOUT_TITLE="Powerpoint Name of layout in Master ...."
+export GTTG_PPTX_LAYOUT_CONTENT="Powerpoint Name of layout in Master ...."
+export GTTG_PPTX_LAYOUT_LASTPAGE="Powerpoint Name of layout in Master ...."
